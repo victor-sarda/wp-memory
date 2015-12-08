056ce643e403a282fe3684655f724d5e8643b60b
Zarryos <flo.louvet94@gmail.com>
2015-12-08 11:54:59 +0100

    Création projet

---
+++ WP-Memory/WP-Memory/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WP_Memory.Resources;
+
+namespace WP_Memory
+{
+    /// <summary>
+    /// Permet d'accéder aux ressources de chaîne.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
